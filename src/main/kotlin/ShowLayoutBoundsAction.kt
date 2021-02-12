099d2192e4f379bfb32691e218df2c1c9b628188
palka <7paulina.sadowska@gmail.com>
2021-02-12 20:06:55 +0100

    01 show notification

--- src/main/kotlin/ShowLayoutBoundsAction.kt
+++ src/main/kotlin/ShowLayoutBoundsAction.kt
@@ -1,33 +1,19 @@
+import com.intellij.notification.NotificationDisplayType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.android.ddmlib.IDevice
-import com.android.ddmlib.NullOutputReceiver
-import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationType
-import com.intellij.openapi.project.Project
-import org.jetbrains.android.sdk.AndroidSdkUtils
 
 
 class ShowLayoutBoundsAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
-        val devices = event.project?.let { AndroidSdkUtils.getDebugBridge(it)?.devices }
-
-        if (devices.isNullOrEmpty()) {
-            event.project?.showNotification("no device connected")
-        } else {
-            event.project?.showNotification("${devices.size} device(s) connected")
-        }
+        NotificationGroup("layout bounds", NotificationDisplayType.BALLOON, true)
+            .createNotification(
+                "Layout bounds plugin says:",
+                "Hello World!",
+                NotificationType.WARNING,
+                null
+            ).notify(event.project)
     }
-}
-
-fun Project.showNotification(message: String) {
-    NotificationGroup("layout bounds", NotificationDisplayType.BALLOON, true)
-        .createNotification(
-            "Layout bounds plugin says:",
-            message,
-            NotificationType.WARNING,
-            null
-        ).notify(this)
 }
